bc645097a016f5a5dcfc1e07b0e98147a35d6df7
Timofey Krischenko <tima.krischenko@gmail.com>
2019-08-22 22:40:23 +0700

    create db, migration, seeds, factoris

---
+++ database/factories/NewsFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\News;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+
+$factory->define(News::class, function (Faker $faker) {
+    return [
+        'title' => $faker->jobTitle,
+        'body' => $faker->text,
+        'categories_id' => rand(1, 5),
+    ];
+});
